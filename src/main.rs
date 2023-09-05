0d5eedb5bf480499dbe689bd395cb7f961ea3937
aakarshan-raj <aakarshan7raj@gmail.com>
2023-09-05 22:25:41 +0530

    values segregated

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::fs;
 
 
-const PATH:&str = "/proc/memoinfo";
+const PATH:&str = "/proc/meminfo";
 
 fn main() {
     let memory_info = fs::read_to_string(PATH).unwrap();
-    println!("{}",memory_info);
+    for lines in memory_info.lines(){
+        let mut split = lines.split_whitespace();
+        let (key,value) = (split.next().unwrap(),split.next().unwrap());
+        println!("key is {} value is {}",key,value);
+
+    }
 }
